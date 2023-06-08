f6e017ce36d82a4bf464bf73fac8ca2f622d906e
belaja-akacija <BA@pm.me>
2023-06-08 14:11:22 -0700

    Initial commit for vim-help-file-maker idea

---
+++ src/lisp/help-file-maker.lisp
@@ -0,0 +1,3 @@
+;;;; Generates the template for the help file, based on interactive input.
+;;;; Ask for heading (name.txt       *name.txt*)
+;;;; Table of contents (if provided, generate none if just blank)

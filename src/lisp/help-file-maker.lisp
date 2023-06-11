2c572c63f66950f8aa0c79b589a9d3a6ecbd0113
belaja-akacija <BA@pm.me>
2023-06-10 22:42:07 -0700

    create basic table of contents generator

--- src/lisp/help-file-maker.lisp
+++ src/lisp/help-file-maker.lisp
@@ -3,15 +3,11 @@
 ;;;; Table of contents (if provided, generate none if just blank)
 
 (defparameter *test-dir* #P "../../tests")
+(defparameter *separator-equal* #\=)
+(defparameter *separator-dash* #\-)
 
-;; TODO make separator terminate at the last column of the header. e.g.:
-;;                                            v
-;;*test.txt*                   some description
-;;=============================================
-;;                                            ^
-(defparameter *separator-equal* "============================================================")
-(defparameter *separator-dash* "------------------------------------------------------------")
-
+;; are these functions really necessary to be separated from one another?
+;; maybe use (labels)?
 (defun get-title-user ()
   (format t "Enter title: ")
   (let ((title (read-line)))
@@ -22,10 +18,20 @@
   (let ((description (read-line)))
     description))
 
-(defun generate-top-of-file (title description)
-  (let ((test-title title)
-        (test-description description)) 
-    (format nil "*~A*~60T~A" test-title test-description)))
+(defun generate-top-of-file ()
+  (let ((title (get-title-user))
+        (description (get-description))) 
+    (format nil "*~A*~60T~A" title description)))
 
-(defun make-template (header toc)
-  (format t "~A~%~A" header toc))
+(defun generate-table-of-contents ()
+  (print "Enter table of contents.")
+  (let ((steps '()))
+    (loop while (not (string= (car steps) ""))
+          do (princ "> ")
+          do (push (read-line) steps))
+    (setf steps (reverse steps))
+    steps))
+
+(defun make-template (header)
+  (let ((separator (make-string (length header) :initial-element *separator-equal*)))
+    (format t "~A~%~A" header separator)))
